12a37b150816e36fe0a7a77f6fc275b6d2c0b14f
kait9110 <71718232+kait9110@users.noreply.github.com>
2020-12-02 14:02:31 -0600

    api key added

--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./weather.css";
 
 export default function Weather() {
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse(response) {}
+
+  const apiKey = "06c11d11419debba7ff8c2da3a596e82";
+  let city = "Austin";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="Weather">
       <form>
@@ -25,7 +34,7 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="Cloudy"
           />
-          <span className="temperature">61</span>
+          <span className="temperature">{temperature}</span>
           <span className="unit">°F</span>
         </div>
         <div className="col-6">
