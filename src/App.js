1721a819205f329126b7842c3b3d136d85d1368f
kait9110 <71718232+kait9110@users.noreply.github.com>
2020-12-02 11:01:25 -0600

    bootstrap added via terminal

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a
-          href="https://github.com/kait9110/react-weather-app-final"
-          target="_blank"
-        >
-          Open-source Code
-        </a>{" "}
-        by Kaitlyn Herring
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          <a
+            href="https://github.com/kait9110/react-weather-app-final"
+            target="_blank"
+          >
+            Open-source Code
+          </a>{" "}
+          by Kaitlyn Herring
+        </footer>
+      </div>
     </div>
   );
 }
