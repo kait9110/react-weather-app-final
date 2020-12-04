82ab81befd0a16d9675aca663dfe1333fbaec727
kait9110 <71718232+kait9110@users.noreply.github.com>
2020-12-04 15:45:20 -0600

    added animated icons to project

--- src/weather.js
+++ src/weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
     });
     setLoaded(true);
